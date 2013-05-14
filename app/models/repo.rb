4de5c775c5f89f718610d1acc96e6004c604242b
Blair Anderson <blair81@gmail.com>
2013-05-13 23:29:00 -0600

    typeahead is complete. IT should be optimized in a background worker but still really slick

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -26,34 +26,4 @@ private
     result.split('/')
   end
 
-
-  # def self.translate_all_to_ruby(repos)    
-  #   repos.each do |repo|
-  #     translate_to_ruby(repo)
-  #   end
-  # end
-
-  # def self.translate_to_ruby(repo)
-  #   Repo.create(
-  #     name: repo.name,
-  #     owner: repo.owner.login,
-  #     url: repo.html_url)
-  # end
-
-  # def add_to_project(project)
-  #   self.project = project
-  #   self.save
-  #   self
-  # end
-
-  # private
-
-  # # This needs to be likely reworked into new class
-  # # Example use in rails console:
-  # # repos = Repo.api_repos(User.last)
-  # def self.api_repos(user = current_user)
-  #   Github.new(
-  #     oauth_token: user.token,
-  #     ssl: {:verify => false}).repos.all
-  # end
 end
