0013d9adc06813f9659ded6fb8efc3b174d32f88
Geoff Schorkopf <gschorkopf@gmail.com>
2013-05-14 18:48:02 -0600

    Added basic tab carousel to projects list

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $('#myModal').modal()
+
+  $('#projectTabs a:first').tab('show');
+
+  $('li.side-nav').tooltip();
+
+  function startCarousel(){
+    delay = 2000; // We'll wanna set this as the data-field for some form
+    
+    var tabCarousel = setInterval(function() {
+      var tabs = $('#projectTabs > li'),
+          active = tabs.filter('.active'),
+          next = active.next('li'),
+          toClick = next.length ? next.find('a') : tabs.eq(0).find('a');
+
+      toClick.trigger('click');
+  }, delay); 
+  };
+
+  function stopCarousel(){
+    clearInterval(tabCarousel)
+  };
+
+  startCarousel(); 
+});
