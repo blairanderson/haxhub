7bfe515b2aaa539c9927c5c2cbea8d6166722080
novohispano@gmail.com <novohispano@gmail.com>
2013-05-17 17:37:40 -0600

    Planner stories tests are passing

--- spec/models/planner_story_spec.rb
+++ spec/models/planner_story_spec.rb
@@ -6,38 +6,31 @@ describe PlannerStory do
   end
 
   it "fetches all stories for a given Pivotal Tracker project" do
-    VCR.use_cassette('fetch_all_stories') do
-      stories = PlannerStory.fetch_all_stories('820647')
-      result  = stories.first
+    VCR.use_cassette('build_planner_stories') do
+      planner = Planner.fetch_project('820647')
+      stories = PlannerStory.fetch_all_stories(planner)
+      result  = stories.last
       expect(stories).to_not be nil
-      expect(stories.count).to eq 22
+      expect(stories.count).to eq 23
       expect(result.project).to eq "SOFTLINE"
       expect(result.requester).to eq "Blair Anderson"
-      expect(result.message).to eq "User visits Home Page\n\nAfter clicking the \"Start Here\" Button, they are sent to auth with github. \n\nAfter auth, their account is created and they're redirected to the root_path"
-    end
-  end
-
-  it "fetches a Pivotal Tracker project" do
-    VCR.use_cassette('fetch_all_stories') do
-      project = PlannerStory.fetch_project('820647')
-      result  = project.name
-      expect(result).to eq "SOFTLINE"
+      expect(result.message).to eq "Majority of API access should be done in the background processes. \n\nThey should check for new data and create it when necessary"
     end
   end
 
   it "fetches all stories related to a project" do
-    VCR.use_cassette('fetch_all_stories') do
-      project = PlannerStory.fetch_project('820647')
-      result  = project.stories.all.count
-      expect(result).to eq 22
+    VCR.use_cassette('build_planner_stories') do
+      planner = Planner.fetch_project('820647')
+      result  = planner.stories.all.count
+      expect(result).to eq 23
     end
   end
 
   it "buils a story from a project" do
-    VCR.use_cassette('fetch_all_stories') do
-      project = PlannerStory.fetch_project('820647')
-      story   = project.stories.all.first
-      result  = PlannerStory.build_story(project, story)
+    VCR.use_cassette('build_planner_stories') do
+      planner = Planner.fetch_project('820647')
+      story   = planner.stories.all.first
+      result  = PlannerStory.build_story(planner, story)
       expect(result.class).to eq PlannerStory
     end
   end
