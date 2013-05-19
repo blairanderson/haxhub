4eb4e945848e2fb5db928b1ea285d9183dac1bd9
novohispano@gmail.com <novohispano@gmail.com>
2013-05-18 18:20:42 -0600

    Minor change in navbar to display the user

--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -2,7 +2,7 @@ module AuthenticationHelper
 
   def auth_message
     if current_user
-      link_to 'Logout', logout_path
+      link_to "#{current_user.login} - logout", logout_path
     else
       link_to "Sign in with Github", github_auth_path
     end
