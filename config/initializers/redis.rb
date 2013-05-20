3e014ac918112c15d2f3901029ab2f68308f6532
Blair Anderson <blair81@gmail.com>
2013-05-19 19:04:40 -0600

    redis stuff

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,12 +1,12 @@
 if Rails.env == 'production'
   uri = URI.parse(ENV["REDISTOGO_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  Resque.redis = 'localhost:6379'
+  Resque.redis.namespace = "ns"
 end
 
-require 'resque_scheduler'
 
-Resque.redis = 'localhost:6379'
-Resque.redis.namespace = "ns"
 
 # If you want to be able to dynamically change the schedule,
 # uncomment this line.  A dynamic schedule can be updated via the
@@ -16,8 +16,8 @@ Resque.redis.namespace = "ns"
 # Note: This feature is only available in >=2.0.0.
 #Resque::Scheduler.dynamic = true
 
-Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
+# Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
 
 # The schedule doesn't need to be stored in a YAML, it just needs to
 # be a hash.  YAML is usually the easiest.
-Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedule.yml'))
+# Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedule.yml'))
