c8b9fea3334dbc335e051e247d77b8afb11f6fc0
Blair Anderson <blair81@gmail.com>
2013-05-19 19:38:21 -0600

    please, maybe, possibly????????????

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,11 +3,11 @@ class GithubService
   def self.prepare(params = {})
     github_auth = {}
     if Rails.env == "production"
-      github_auth[:redirect_uri] = github_callback_url
+      github_auth[:redirect_uri] = "http://feed-softline.herokuapp.com/auth/github/callback"
     end
 
-    github_auth[:client_id] = '81d9e96eebf415c911d1' #this is temporary, couldn't git github.yml to send info
-    github_auth[:client_secret] = "f539d89fc3d4735ed65d827db173fa778d9fdb84"
+    github_auth[:client_id] = ENV['GITHUB_ID'] ||'81d9e96eebf415c911d1' #this is temporary, couldn't git github.yml to send info
+    github_auth[:client_secret] = ENV['GITHUB_SECRET'] || 'f539d89fc3d4735ed65d827db173fa778d9fdb84'
     github_auth[:ssl] = {:verify => false}
     if params[:github_token]
       github_auth[:oauth_token] = params[:github_token]
