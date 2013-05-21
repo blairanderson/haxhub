dfc2ac59539ea42174df623b9d15973b7be60947
Geoff Schorkopf <gschorkopf@gmail.com>
2013-05-20 22:20:05 -0600

    Made more descriptive method name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :current_user, :logged_in?, :first_data
+  helper_method :current_user, :logged_in?, :first_li_event_at
 
   def current_user
     @current_user ||= User.find_by_login(session[:login])
