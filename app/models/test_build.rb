03d29fd654b1cac8a60dc291d284424f36bef3bc
Blair Anderson <blair81@gmail.com>
2013-05-21 21:51:48 -0600

    fixing the test builds

--- app/models/test_build.rb
+++ app/models/test_build.rb
@@ -24,21 +24,25 @@ class TestBuild < ActiveRecord::Base
   end
 
   def self.create_from(build, ci_source_id)
-    new_build = where( build_id: build.number, 
-                       ci_source_id: ci_source_id).first_or_create
-    new_build.attributes = {
-      author:       build.commit.author_name, 
-      commit:       build.commit_id,
-      committer:    build.commit.committer_name,
-      compare:      build.commit.compare_url, 
-      config:       build.config[:rvm],
-      duration:     build.duration,
-      finished:     build.finished_at,
-      started:      build.started_at,
-      message:      build.commit.message, 
-      state:        build.state
-    }
-    new_build.save
-    new_build
+    begin
+      new_build = where( build_id: build.number, 
+                         ci_source_id: ci_source_id).first_or_create
+      new_build.attributes = {
+        author:       build.commit.author_name, 
+        commit:       build.commit_id,
+        committer:    build.commit.committer_name,
+        compare:      build.commit.compare_url, 
+        config:       build.config[:rvm],
+        duration:     build.duration,
+        finished:     build.finished_at,
+        started:      build.started_at,
+        message:      build.commit.message, 
+        state:        build.state
+      }
+      new_build.save
+      new_build
+    rescue
+
+    end
   end
 end
